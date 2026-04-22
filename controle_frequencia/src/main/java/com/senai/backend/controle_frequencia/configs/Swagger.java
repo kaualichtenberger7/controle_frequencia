151a81aa2baf476a6072b0fe4124c173fad5c69e
Kaua Lichtenberger Furtado de Souza <pet0163519@aluno.firjansenaisesi.com.br>
2026-04-22 16:26:28 -0300

    controle

---
+++ controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/configs/Swagger.java
@@ -0,0 +1,18 @@
+package com.senai.backend.controle_frequencia.configs;
+
+public class Swagger {
+
+public Swagger() {
+    }
+
+@Configuration
+@OpenAPIDefinition(
+info = @Info(
+title = "Insira o nome da sua API",
+version = "1.0",
+description = "Insira a descrição da sua API"
+)
+)
+public class Swagger {
+}
+}
